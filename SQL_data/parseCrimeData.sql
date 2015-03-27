86e459538f068444847111cd1ebc20e1398447bb
ccc343 <cissychen0034@gmail.com>
2015-03-27 18:09:40 -0400

    Sql scripts for crime data

---
+++ SQL_data/parseCrimeData.sql
@@ -0,0 +1,19 @@
+﻿
+/* Parses..csv file with information about crimes - each "data point" includes the longitude/latitude location,
+type of crime, the number of crimes of the type, and the month/year in which they ocurred. */
+
+CREATE TABLE "nyc_crime_2014"
+(
+	longitude double precision,
+	latitude double precision,
+	YR int,
+	MO int,
+	X int,
+	Y int,
+	TOT int,
+	CR char(100)
+);
+
+COPY "nyc_crime_2014" (longitude, latitude, YR, MO, X, Y, TOT, CR) FROM 'C:\Users\CCHEN\Desktop\Princeton Junior\COS 333\Project\data\nyc_2014.csv' HEADER DELIMITER ',' CSV;
+
+ALTER TABLE "nyc_crime_2014" ADD COLUMN id BIGSERIAL PRIMARY KEY;
---
+++ SQL_data/selectRadiusData.sql
@@ -0,0 +1,5 @@
+﻿SELECT *, point(-73.9939, 40.7506) <@> point (longitude, latitude)::point AS distance
+FROM nyc_crime_2014
+/* Within a mile */
+WHERE (point(-73.9939, 40.7506) <@> point(longitude, latitude)) < 1
+ORDER by distance;
